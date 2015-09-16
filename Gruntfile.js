4733cc5168eaf9df1f029ffa017caaafe23bd10d
Thomas Meehan <thomas.meehan@living-group.com>
2015-09-16 18:42:13 +0100

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        settings: {
+            assets: 'assets',
+            dist: 'dist',
+            templates: 'templates',
+            temp: '.tmp'
+        },
+        banner: '/*!\n' +
+        '* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '*/\n',
+        clean: {
+            build: ["<%= settings.dist %>/css"]
+        },
+        less: {
+            build: {
+                options: {
+                    paths: [
+                        "<%= settings.assets %>/less"
+                    ]
+                },
+                files: {
+                    "wwwroot/styles/main.css": "<%= settings.assets %>/less/main.less"
+                }
+            },
+        },
+        copy: {
+            fonts: {
+                files: [
+                    {expand: true, cwd: '<%= settings.assets %>/fonts/', src: ['**'], dest: '<%= settings.dist %>/fonts/'},
+                ],
+            },
+        },
+        watch: {
+            less: {
+                files: ['<%= settings.assets %>/less/**/*.less'],
+                tasks: ['less'],
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', [
+        'clean',
+        'less',
+        'copy'
+    ]);
+
+
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+Bare Minimum Grunt
+==================
+
+The really basic grunt setup I use to start projects.
+
+Add more features as needed and update versions regularly.
+
+  npm install
+
+  grunt
